5f0618e5c81aec71776f340f0113f38e53ac19af
SaulGoodmn <44654549+SaulGoodmn@users.noreply.github.com>
2022-05-22 16:39:53 -0400

    initial commit

---
+++ src/test/java/MFD/HealthManagementSystem/HealthManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package MFD.HealthManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
